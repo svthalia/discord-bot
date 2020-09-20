40ad9e860a2fbeca6289c1ee6078486b72847937
Sébastiaan <se_bastiaan@outlook.com>
2020-09-20 15:53:03 +0200

    Add on_error for main bot

--- bot/src/main.py
+++ bot/src/main.py
@@ -13,18 +13,27 @@ load_dotenv()
 
 import discord
 from discord.ext import commands
+
 from cogs.greetings import GreetingsCog
 from cogs.whoami import WhoAmICog
+
+from bot_logger import get_logger
 
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 GUILD_ID = os.getenv("DISCORD_GUILD_ID")
 
 bot = commands.Bot(command_prefix="!")
+logger = get_logger(__name__)
 
 
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} has connected to Discord!")
+
+
+@bot.event
+async def on_error(event, *args, **kwargs):
+    logger.warning(traceback.format_exc())
 
 
 bot.add_cog(GreetingsCog(bot))
