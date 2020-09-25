9cc5462fec4313d8ada181fa285f7971f8cf531a
Sébastiaan <se_bastiaan@outlook.com>
2020-09-25 14:55:42 +0200

    Add more API helpers (#15)

--- common/thalia_api.py
+++ common/thalia_api.py
@@ -12,3 +12,13 @@ async def get_authenticated_member(client):
 async def get_member_by_id(client, user_id):
     response = await client.get(f"{THALIA_API_URL}/members/{user_id}/")
     return response.json()
+
+
+async def get_membergroups(client):
+    response = await client.get(f"{THALIA_API_URL}/activemembers/groups/")
+    return response.json()
+
+
+async def get_members(client):
+    response = await client.get(f"{THALIA_API_URL}/members/")
+    return response.json()
