2edc1d0ade2e990e1b7e5d03cee4187034d713ef
Sébastiaan <se_bastiaan@outlook.com>
2020-09-25 14:21:51 +0200

    Add autoremove functionality for the help command (#16)

--- bot/core/bot.py
+++ bot/core/bot.py
@@ -1,12 +1,13 @@
 from discord.ext import commands
 from common.thalia_oauth import get_backend_oauth_client
+from .help import ThaliaHelpCommand
 
 
 class ThaliaBot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.thalia_client = None
-        self.help_command = commands.DefaultHelpCommand(dm_help=True)
+        self.help_command = ThaliaHelpCommand()
 
     async def connect(self, *args, **kwargs):
         self.thalia_client = await get_backend_oauth_client()
