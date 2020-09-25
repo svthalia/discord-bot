2c0c8b0cda32bc9bf14ccb34016a8ed3d3721886
Sébastiaan <se_bastiaan@outlook.com>
2020-09-25 11:39:30 +0200

    Add more API helpers

--- common/ddb.py
+++ common/ddb.py
@@ -1,3 +1,5 @@
+from typing import List, Union
+
 import os
 import aioboto3
 
@@ -11,7 +13,7 @@ USERS_TABLE = os.getenv("USERS_TABLE")
 logger = get_logger(__name__)
 
 
-async def write_user(thalia_user_id, discord_user_id):
+async def write_user(thalia_user_id: Union[str, int], discord_user_id: Union[str, int]):
     async with aioboto3.resource("dynamodb") as dynamodb:
         users_table = await dynamodb.Table(USERS_TABLE)
         response = await users_table.update_item(
@@ -24,7 +26,7 @@ async def write_user(thalia_user_id, discord_user_id):
         return response
 
 
-async def get_user_by_thalia_id(thalia_user_id):
+async def get_user_by_thalia_id(thalia_user_id: Union[str, int]) -> dict:
     async with aioboto3.resource("dynamodb") as dynamodb:
         users_table = await dynamodb.Table(USERS_TABLE)
         try:
@@ -38,7 +40,7 @@ async def get_user_by_thalia_id(thalia_user_id):
             return response["Items"][0] if response["Count"] > 0 else None
 
 
-async def get_user_by_discord_id(discord_user_id):
+async def get_user_by_discord_id(discord_user_id: Union[str, int]) -> dict:
     async with aioboto3.resource("dynamodb") as dynamodb:
         users_table = await dynamodb.Table(USERS_TABLE)
         try:
@@ -51,3 +53,17 @@ async def get_user_by_discord_id(discord_user_id):
             return None
         else:
             return response["Items"][0] if response["Count"] > 0 else None
+
+
+async def get_discord_users_by_thalia_ids(thalia_user_ids: List[str]) -> List[dict]:
+    async with aioboto3.resource("dynamodb") as dynamodb:
+        users_table = await dynamodb.Table(USERS_TABLE)
+        try:
+            response = await users_table.query(
+                KeyConditionExpression=Key("thalia_user_id").is_in(thalia_user_ids)
+            )
+        except ClientError as e:
+            logger.error(e.response["Error"]["Message"])
+            return None
+        else:
+            return response["Items"] if response["Count"] > 0 else None
