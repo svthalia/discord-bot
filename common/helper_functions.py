8b9f99928e01425a9cb45a9bbd668b3ca5e521de
mboute <m.boute@student.science.ru.nl>
2020-10-02 13:25:30 +0200

    Removed warnings caused by PMs

--- common/helper_functions.py
+++ common/helper_functions.py
@@ -8,8 +8,9 @@ async def reply_and_delete(ctx, reply: str):
         await ctx.author.send(reply)
         await ctx.message.delete()
     except:
-        logger.error(
-            "Could not reply to/remove old message: 'ctx.message.content=%s' by user: 'ctx.author=%s'",
-            ctx.message.content,
-            ctx.author,
-        )
+        if ctx.guild:
+            logger.warning(
+                "Could not reply to/remove old message: 'ctx.message.content=%s' by user: 'ctx.author=%s'",
+                ctx.message.content,
+                ctx.author,
+            )
