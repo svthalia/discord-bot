347cf9a2d2ec091ae4976a04a9a4601b8db4249c
Mark Boute <33718602+mark-boute@users.noreply.github.com>
2020-10-02 13:38:05 +0200

    Add function to delete and reply to message and apply this function i… (#19)
    * Add function to delete and reply to message and apply this function in cogs/member.py
    
    * Update helper_functions.py
    
    * Removed warnings caused by PMs

---
+++ common/helper_functions.py
@@ -0,0 +1,16 @@
+from common.bot_logger import get_logger
+
+logger = get_logger(__name__)
+
+
+async def reply_and_delete(ctx, reply: str):
+    try:
+        await ctx.author.send(reply)
+        await ctx.message.delete()
+    except:
+        if ctx.guild:
+            logger.warning(
+                "Could not reply to/remove old message: 'ctx.message.content=%s' by user: 'ctx.author=%s'",
+                ctx.message.content,
+                ctx.author,
+            )
