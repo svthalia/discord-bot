cb5349ff45060d72a51b0e1246825768a3a8e8b0
Sébastiaan <se_bastiaan@outlook.com>
2020-10-06 19:39:17 +0200

    Do a filter on user ids in Python instead of the filter expression

--- common/ddb.py
+++ common/ddb.py
@@ -73,4 +73,5 @@ async def get_discord_users_by_thalia_ids(thalia_user_ids: List[str]) -> List[di
             logger.error(e.response["Error"]["Message"])
             return None
         else:
+            data = [member for member in data if member["thalia_user_id"] in thalia_user_ids]
             return data if len(data) > 0 else None
