43ad2c46dc3113fef84ec891dd0560bfecc2ba9e
Sébastiaan <se_bastiaan@outlook.com>
2021-07-02 11:57:24 +0200

    Implement usage of Thalia Concrexit API v2

--- common/thalia_api.py
+++ common/thalia_api.py
@@ -1,7 +1,8 @@
 import os
+import math
 
 THALIA_SERVER_URL = os.getenv("THALIA_SERVER_URL")
-THALIA_API_URL = f"{THALIA_SERVER_URL}api/v1"
+THALIA_API_URL = f"{THALIA_SERVER_URL}api/v2"
 
 
 async def get_authenticated_member(client):
@@ -15,10 +16,41 @@ async def get_member_by_id(client, user_id):
 
 
 async def get_membergroups(client):
-    response = await client.get(f"{THALIA_API_URL}/activemembers/groups/")
-    return response.json()
+    groups = _get_paginated_results(f"{THALIA_API_URL}/activemembers/groups/")
+    coros = [_get_individual_group(group["pk"]) for group in groups]
+    return asyncio.gather(*coros)
 
 
 async def get_members(client):
-    response = await client.get(f"{THALIA_API_URL}/members/")
-    return response.json()
+    return _get_paginated_results(f"{THALIA_API_URL}/members/")
+
+
+async def _get_paginated_results(client, url):
+    location = f"{url}?limit=100"
+
+    response = await client.get(location)
+    data = response.json()
+
+    if data["count"] > 100:
+        num = math.ceil(data["count"] / 100)
+
+        async def get_next(url, i):
+            response = await client.get(f"{url}?limit=100&offset={100*i}")
+            data = response.json()
+            return data["results"]
+
+        coros = [get_next(url, i) for i in range(1, num)]
+        more_data = await asyncio.gather(*coros)
+
+        data += [item for sublist in more_data for item in sublist]
+
+    return data
+
+
+async def _get_individual_group(pk):
+    response = await client.get(f"{THALIA_API_URL}/activemembers/groups/{pk}")
+    data = response.json()
+    chairs = filter(lambda x: x["chair"], data["members"])
+    if len(chairs) > 0:
+        data["chair"] = chair[0]
+    return data
